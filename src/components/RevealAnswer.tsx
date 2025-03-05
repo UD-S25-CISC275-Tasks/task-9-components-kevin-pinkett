293f18ece91d66ecb93e51ee80970fe7ed6c7956
Kevin Pinkett <kpinkett@udel.edu>
2025-03-05 15:26:23 -0500

    RevealAnswer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+    const answer: string = "42";
+
+    function flipVisible(): void {
+        setVisible(!visible)
+    }
+
+    return <div>
+        <span>
+            <Button onClick={flipVisible}>Reveal Answer</Button>
+        </span>
+        {visible && answer}
+    </div>;
 }
