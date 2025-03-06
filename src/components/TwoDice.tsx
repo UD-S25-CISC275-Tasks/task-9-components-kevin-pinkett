4ca14f220e89c90e1e5d6bb4952c99cc1a7be656
Kevin Pinkett <kpinkett@udel.edu>
2025-03-05 21:21:35 -0500

    TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,32 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [left, setLeft] = useState<number>(3)
+    const [right, setRight] = useState<number>(2)
+
+
+    function rollLeft(): void {
+        setLeft(d6)
+    }
+
+    function rollRight(): void {
+        setRight(d6)
+    }
+
+    return (
+        <div>
+            <span data-testid="left-die">
+                Left Die: {`${left}`}
+                <Button onClick={rollLeft}>Roll Left</Button>
+            </span>
+            <span data-testid="right-die">
+                Right Die: {`${right}`}
+                <Button onClick={rollRight}>Roll Right</Button>
+            </span>
+            <span>
+                {left == right && left != 1 && "Win"}
+                {left == right && left == 1 && "Lose"}
+            </span>
+        </div>
+    );
 }
